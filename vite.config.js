0a101f6cb40a54fbacfb20543d41737cbd5c82a3
40David <vishwasgh2004@gmail.com>
2025-10-06 19:21:32 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '/sensor-data/ESP32_001': {
         target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/sensor-data/ESP32_001/ '/dashboard'),
+        rewrite: (path) => path.replace(/^\/sensor-data\/ESP32_001/, '/dashboard'),
       },
        '/predict': {
         target: process.env.VITE_BACKEND_URL,
